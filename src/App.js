fed19e2203d80860da1d89f184c7f2e99769ffc9
Laura <rokey.laura@gmail.com>
2019-04-16 11:22:10 -0500

    working version from example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Adder } from './Adder.js';
-import { Counter } from './Counter.js';
-import { ProgressBar } from './ProgressBar.js';
-import { Editor } from './Editor.js';
-import { CountWords } from './helpers.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		howManyWords: "",
+  		secondValue: "",
+  		needWords: "",
+  		wordCount: "",
+  		limWords: null
+  	}
+  	this.firstHandle = this.firstHandle.bind(this);
+  	this.handleSubmit = this.handleSubmit.bind(this);
+  	this.secondHandle = this.secondHandle.bind(this);
+  }
+
+  firstHandle(e) {
+  	this.setState({
+  		howManyWords: e.target.value
+  	});
+  }
+
+  handleSubmit(e) {
+  	e.preventDefault();
+  	this.setState ({
+  		needWords: this.state.howManyWords,
+  		secondValue: ""
+  	});
+  }
+
+  secondHandle(event) {
+  	console.log("pp", this.state.needWords, this.state.wordCount);
+  	const wordCount = event.target.value === "" ? 0 : event.target.value.split(" ").length;
+  	this.setState({
+  		secondValue: event.target.value,
+  		wordCount: wordCount,
+  		limWords: this.state.needWords - wordCount < 0 ? this.state.secondValue.length : null
+  	});
+  }
+
   render() {
+  	let result = this.state.needWords - this.state.wordCount;
+  	let tooManyWords;
+  	if (result < 0) {
+  		const tooManyWordsStyle = {
+  			color: "red"
+  		};
+  		tooManyWords = (
+  			<p style={tooManyWordsStyle}>
+  				You exceeded the maximum number of words!
+  			</p>
+  		);
+  	}
     return (
     	<div>
-	      	<Adder n1={5} n2={5} />
-	      	<Counter count={2000} />
-	      	<ProgressBar completion={.5} />
-	      	<Editor text={"hi hi hi hi hi hi hi hi"} />
+    		<form onSubmit={this.handleSubmit}>
+    			<p>How many words do you have to write?</p>
+    			<input
+    				type="text"
+    				value={this.state.howManyWords}
+    				onChange={this.firstHandle}
+    			/>
+    			<button type="submit">Go</button>
+    		</form>
+    		<form>
+    			<p>You still have to write {result} words!</p>
+    			<textarea
+    				type="text"
+    				value={this.state.secondValue}
+    				onChange={this.secondHandle}
+    				maxLength={this.state.limWords}
+    			/>
+    			{tooManyWords}
+    		</form>
 	    </div>
     );
   }
--- src/helpers.js
+++
@@ -1,3 +0,0 @@
-​export function​ countWords(text) {
-​     //return​ text ? text.match(​/​​\w​​+/g​).length : 0;
-​}
